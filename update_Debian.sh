65e3149e313fee29d86e3f27f811ede45e97df69
sampablo22 <sam.mcmaster79@gmail.com>
2020-11-11 02:54:15 +0000

    Update update_Debian.sh
    Added spacing in asking for input command

--- update_Debian.sh
+++ update_Debian.sh
@@ -2,7 +2,7 @@
 # This is a script to update the repositories and operating system for Debain based operating systems
 echo "Hello "$USER", your operating system and repositories will now be updated."
 while true; do
-	read -p "Do you wish to run the update command?" yn
+	read -p "Do you wish to run the update command? " yn
 	case $yn in
 		[Yy]* ) sudo apt update; echo "Thanks for updating me."; break;;
 		[Nn]* ) break;;
@@ -10,7 +10,7 @@ while true; do
 	esac
 done
 while true; do
-	read -p "Do you wish to run the upgrade command?" yn
+	read -p "Do you wish to run the upgrade command? " yn
 	case $yn in
 		[Yy]* ) sudo apt upgrade; echo "Thanks for updating me."; break;;
 		[Nn]* ) break;;
@@ -18,7 +18,7 @@ while true; do
 	esac
 done
 while true; do
-	read -p "Do you wish to run the full-upgrade command?" yn
+	read -p "Do you wish to run the full-upgrade command? " yn
 	case $yn in
 		[Yy]* ) sudo apt full-upgrade; echo "Thanks for updating me."; break;;
 		[Nn]* ) break;;
